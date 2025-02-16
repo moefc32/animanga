82fe10ed91bdcf04a85a52d262571aac94dcc174
Faizal Chan <moefc32@gmail.com>
2025-02-17 04:37:11 +0700

    add .prettierrc and fix indentations

--- src/lib/component/Title.svelte
+++ src/lib/component/Title.svelte
@@ -1,12 +1,12 @@
 <script>
-  export let count = 191;
+    export let count = 191;
 </script>
 
 <section
-  class="mb-5 px-3 pt-[120px] pb-5 bg-[linear-gradient(to_bottom_left,rgba(33,150,243,0.75),rgba(76,175,80,0.75))] bg-fixed text-white text-center h-[250px]"
+    class="mb-5 px-3 pt-[120px] pb-5 bg-[linear-gradient(to_bottom_left,rgba(33,150,243,0.75),rgba(76,175,80,0.75))] bg-fixed text-white text-center h-[250px]"
 >
-  <h1 class="m-3 text-3xl">Anime and Manga Collection</h1>
-  <p class="mx-3">
-    There are currently {count} titles recommended by Mfc
-  </p>
+    <h1 class="m-3 text-3xl">Anime and Manga Collection</h1>
+    <p class="mx-3">
+        There are currently {count} titles recommended by Mfc
+    </p>
 </section>
